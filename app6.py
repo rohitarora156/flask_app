d112ce07653debe10afccdd3c5527c59ba9a6d31
Rohit Arora <ra079174@gmail.com>
2024-06-24 22:34:51 +0530

    created code app

---
+++ app6.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate,identity
+from resources.user import UserRegister
+from resources.item import Item,ItemList
+
+app = Flask(__name__)
+app. config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'Rohit'
+api = Api(app)
+
+jwt = JWT(app,authenticate,identity) #this creates a new endpoint : /auth
+
+    
+api.add_resource(Item,'/item/<string:name>')
+api.add_resource(ItemList,'/items')
+api.add_resource(UserRegister,'/register')
+
+if __name__ == '__main__':
+    from database import db
+    db.init_app(app)
+    app.run(port = 2000,debug = True)
